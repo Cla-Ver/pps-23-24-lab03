59574a8b2feed7b69970b68672a25d9bd88d3bc2
Claudio Verazza - claudio.verazza@studio.unibo.it <claudio.verazza@studio.unibo.it>
2024-03-18 12:06:15 +0100

    Partial ex1 solution

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -25,12 +25,25 @@ object Sequences: // Essentially, generic linkedlists
       case Nil()                 => Nil()
 
     // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
-
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Cons(h1, t1), Cons(h2, t2)) if first != Nil() && second != Nil() => Cons((h1, h2), zip(t1, t2))
+      case _ => Nil()
     
-    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
-    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
+
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
+      case Cons(h, t) if n > 0 => Cons(h, take(t)(n-1))
+      case _ => Nil()
+    
+    def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = (l1, l2) match
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1, concat(t1, Cons(h2, t2)))
+      case (Nil(), Cons(h2, t2)) => Cons(h2, concat(l1, t2))
+      case (Cons(h1, t1), Nil()) => Cons(h1, concat(t1, l2))
+      case _ => Nil()
+    
+    def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = l match
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
+      case _ => Nil()
+    
 
     def min(l: Sequence[Int]): Optional[Int] = ???
     
@@ -42,3 +55,9 @@ object Sequences: // Essentially, generic linkedlists
   import Sequence.*
 
   println(sum(map(filter(l)(_ >= 20))(_ + 1))) // 21+31 = 52
+
+    val l2: Sequence[Int] = Cons(10, Nil())
+    val res = flatMap(l2)(v => Cons(v + 1, Nil()))
+    val e = "i"
+    
+
